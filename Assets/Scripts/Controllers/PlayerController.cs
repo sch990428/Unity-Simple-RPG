1bffad6d7bd3da07a3388485586db63f8299adfd
sch990428 <990428sch@naver.com>
2023-11-26 18:41:13 +0900

    플레이어 이동 구현
    플레이어 이동 구현

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	[SerializeField]
+	float _speed = 10.0f;
     void Start()
     {
         GameManagers gm = GameManagers.GameManager;
@@ -11,6 +13,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
-    }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += Vector3.left * Time.deltaTime * _speed;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position +=  Vector3.back* Time.deltaTime * _speed;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += Vector3.right * Time.deltaTime *	_speed;
+		}
+	}
 }
